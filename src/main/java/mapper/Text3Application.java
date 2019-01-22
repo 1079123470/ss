8be7a05035eed6ddb4cf987bb95890bc677bf61f
wangguangyuan <wangguangyuan@DESKTOP-UTTEPSE>
2019-01-22 09:33:29 +0800

    sss

--- src/main/java/mapper/Text3Application.java
+++ src/main/java/mapper/Text3Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Text3Application {
 
 	public static void main(String[] args) {
+		System.out.println("------");
 		SpringApplication.run(Text3Application.class, args);
 	}
 
